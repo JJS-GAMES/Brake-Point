bfea566d4dd96f48a4338f2fa355280a16db3b30
Daniil <136441101+tohiro15@users.noreply.github.com>
2025-09-22 00:17:02 +0800

    feat: improved barrier system
    - Moved Header to Tooltip in barrier script field
    - Redesigned barrier system: barriers now fly off the map after collision and have a collider with Rigidbody
    - Added more settings in barrier object inspector
    - Minimized risk of barrier malfunction

--- Assets/Scripts/Map/Barrier.cs
+++ Assets/Scripts/Map/Barrier.cs
@@ -1,23 +1,32 @@
+using System.Collections;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(BoxCollider2D), typeof (Rigidbody2D), typeof(Collider2D))]
 public class Barrier : MonoBehaviour
 {
-    [Header("Percentage of car speed deceleration / Процент замедления скорости машины")]
-    [Range(0f, 100f)]
-    [SerializeField] private float _speedReductionPercent = 20f;
-
+    [SerializeField, Range(0f, 100f), Tooltip("Percentage of car speed deceleration / Процент замедления скорости машины")]
+    private float _speedReductionPercent = 20f;
+    [SerializeField, Range(0f, 10f), Tooltip("Time in seconds before the barrier is disabled after collision / Время в секундах до отключения барьера после столкновения.")]
+    private float _deactivationDelay = 5f;
+    [SerializeField, Range(1f, 20f), Tooltip("Barrier mass (kg) / Масса барьера (кг)")]
+    private float _mass = 1f;
     private BoxCollider2D _boxCollider;
+    private Rigidbody2D _rigidbody;
 
     private void Awake()
     {
         _boxCollider = GetComponent<BoxCollider2D>();
-        _boxCollider.isTrigger = true;
+        _boxCollider.enabled = true;
+        _boxCollider.isTrigger = false;
+
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.simulated = true;
+        _rigidbody.mass = _mass;
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        var car = other.GetComponent<CarController>();
+        var car = collision.gameObject.GetComponent<CarController>();
         if (car == null) return;
 
         Rigidbody2D rb = car.GetRb;
@@ -26,6 +35,13 @@ public class Barrier : MonoBehaviour
         float factor = Mathf.Clamp01(1f - _speedReductionPercent / 100f);
         rb.linearVelocity *= factor;
 
-        gameObject.SetActive(false);
+        StartCoroutine(DisableBarrierAfterDelay(_deactivationDelay));
     }
+
+    private IEnumerator DisableBarrierAfterDelay(float delay)
+    { 
+        _boxCollider.enabled = false;
+        yield return new WaitForSeconds(delay);
+        gameObject.SetActive(false);
+    }    
 }
